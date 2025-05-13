19ad272f85e743bac8f797dfba41420097ede12c
Andrii Denysenko <andrii.denysenko@first8.nl>
2025-05-13 17:12:51 +0200

    Quarkus extention - configuration substitution tested

--- extensions/quarkus/deployment/src/test/java/io/carbonintensity/scheduler/quarkus/test/DevModeTest.java
+++ extensions/quarkus/deployment/src/test/java/io/carbonintensity/scheduler/quarkus/test/DevModeTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 
 import io.carbonintensity.scheduler.GreenScheduled;
 import io.quarkus.devui.tests.DevUIJsonRPCTest;
@@ -27,11 +28,19 @@ public class DevModeTest extends DevUIJsonRPCTest {
     public void testGetData() throws Exception {
         JsonNode data = super.executeJsonRPCMethod("getData", Map.of());
         Assertions.assertTrue(data.get("schedulerRunning").asBoolean());
+        ArrayNode methods = data.withArray("methods");
+        Assertions.assertEquals(2, methods.size());
+        Assertions.assertTrue(methods.at("/0/schedules/0/identity").asText().startsWith("the_schedule"));
+        Assertions.assertTrue(methods.at("/1/schedules/0/successive").asText().equals("1S 4S 5S"));
     }
 
     static class Jobs {
         @GreenScheduled(identity = "the_schedule", successive = "1S 4S 5S", duration = "PT1M", zone = "NL")
         void ping() {
+        }
+
+        @GreenScheduled(identity = "the_schedule2", successive = "1S 4S 5S", duration = "PT1M", zone = "NL")
+        void ping2() {
         }
     }
 
